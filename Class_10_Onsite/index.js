b59f242ed6c29c10b33a3d683411358b292fc2c2
Emmad Ahmad Usmani <em.madi.don@gmail.com>
2023-09-03 16:49:40 +0500

    class 10

---
+++ Class_10_Onsite/index.js
@@ -0,0 +1,74 @@
+"use strict";
+console.log("Class 10 Onsite");
+// var userNames: string[] = ["Emad", "Ali", "Yaseen", "Arham"];
+// for (let index = 0; index < userNames.length; index++) {
+// 	const userName = userNames[index];
+// 	console.log("userName", userName);
+// }
+// userNames.forEach((userName: string, Index: number, fullarr: string[]) => {
+// 	console.log("get Value ->", userName);
+// 	console.log("get Value ->", Index);
+// 	console.log("get Value -> fullarr", fullarr);
+// });
+// function mapFunc(value: string, i: number, arr: string[]) {
+// 	if (i % 2 == 0) {
+// 		return i;
+// 	}
+// 	// 	console.log("value", value);
+// 	// 	console.log("index", i);
+// 	// 	console.log("arr", arr);
+// 	// }
+// }
+// var list: number[] = userNames.filter(mapFunc);
+// console.log("list", list);
+// // userNames.forEach(mapFunc);
+// Write a ts Program , That Reshape the input array [2,5,10,20]
+// var list: number[] = [2, 5, 10, 20];
+// var newList: number[] = list.map((element) => element * 2);
+// console.log("newList", newList);
+// string
+// number
+// boolean
+// undefined
+// null
+// array
+// names , rollno , session
+// let names: string[] = ["emad", "yaseen", "ali"];
+// let student1 = ["emad", 2002, "evening"];
+// let student2 = ["yaseen", 2003, "morning"];
+// let student3 = ["ali", "evening", 2004];
+// student1[2];
+// student2[2];
+// student3[2];
+// Objects
+// let student1 = {
+// 	name: "Emad",
+// 	rollNo: 30002,
+// 	session: "Evening",
+// };
+// console.log(student1.name, student1.rollNo, student1.session);
+// let student2 = {
+// 	name: "Yaseen",
+// 	rollNo: 30004,
+// 	session: "Evening",
+// };
+// console.log(student2.name, student2.rollNo, student2.session);
+let car = [
+    {
+        model: "city",
+        brand: "honda",
+        isUsed: false,
+        modelYear: 1992,
+        color: "white",
+    },
+    {
+        model: "7 Series",
+        brand: "BMW",
+        isUsed: false,
+        modelYear: 2023,
+        color: "Black",
+    },
+];
+for (let i = 0; i < car.length; i++) {
+    console.log(car[i].modelYear);
+}
